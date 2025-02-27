86705b3dc4f53224f292b267907fdf8e40858c6e
Guilherme Santana <gucsantana@gmail.com>
2025-02-27 21:40:49 +0900

    current: aborted plan for long-ass scryfall query, has limit

--- src/App.vue
+++ src/App.vue
@@ -903,8 +903,6 @@ async function perform_decklist_finder_search() {
   decklist_finder_results.value = []
   decklist_finder_results_processed.value = []
 
-  var scryfall_query = "https://api.scryfall.com/cards/search?q="
-
   // first, we split the list of cards to search, one per line
   var split_cards = decklist_finder_text.split('\n')
 
@@ -949,7 +947,6 @@ async function perform_decklist_finder_search() {
 
       // return a list of all instances of the named card in your collection, optionally narrowing by set and/or number
       let cardsFound = findSpecificCardInCollection(card.name, card.set != '' ? card.set : undefined, card.collector_number != 0 ? card.collector_number : undefined)
-      for(var card in cardsFound)
       // if search priority is newest cards first, we revert the (normally oldest to newest) array
       if(decklist_finder_priority.value == "newest") cardsFound = cardsFound.reverse()
       // add all of the prints found to the search results pile
